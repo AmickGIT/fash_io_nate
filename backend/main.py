07c7cec66277303e33b2b40ce1d487069d5c14d8
AmickGIT <amickpvt@gmail.com>
2025-07-23 23:54:24 +0530

    trying to fix cors issues

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     "https://fash-io-nate.vercel.app",   
     "http://localhost:3000",             
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
