acd147a426828e904816b7e9e4b43c33941d285f
AmickGIT <amickpvt@gmail.com>
2025-08-10 23:11:36 +0530

    added new domain in next.config file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["pkraxwnlcgejwxunkeco.supabase.co"],
+    domains: ["ledozusiwfwfekmbqkzs.supabase.co"],
   },
 };
 
