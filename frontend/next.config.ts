7d37198d4b7295356e023bb33fd1c8a8202e1f68
AmickGIT <amickpvt@gmail.com>
2026-07-07 00:03:23 +0530

    Update Supabase image domain

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["ledozusiwfwfekmbqkzs.supabase.co"],
+    domains: ["tyaixdufejnabeiswhyx.supabase.co"],
   },
 };
 
